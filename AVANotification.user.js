6ee533ec0a6853b785abb07acc72e6eae077cd4b
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2023-11-20 16:57:54 -0600

    ready to release

--- AVANotification.user.js
+++ AVANotification.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         AVANotification b2.0
+// @name         AVANotification r2.0
 // @namespace    http://tampermonkey.net/
 // @version      2.0
 // @description  try to take over the world!
